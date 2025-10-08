fcc0c11f2451f4f6d376f96e3281432b7d0e7548
aditlfp <aditya.budilfp11@gmail.com>
2025-10-08 09:58:24 +0700

    feat: change language EN to ID

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,25 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel/serverless';
+import i18n from 'astro-i18n-aut/integration';
 
 // https://astro.build/config
+const defaultLocale = "id";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  id: "id-ID",
+};
+
 export default defineConfig({
     output: 'server',
     adapter: vercel({}),
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [
+    i18n({
+        locales,
+        defaultLocale,
+    }),
+  ],
 });
